3d170d02a5e1ad03c8b85f6439d294c983e4dcad
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-29 14:01:00 +0300

    uzd

--- static/posts.js
+++ static/posts.js
@@ -1,1 +1,23 @@
 console.log('hello from posts');
+
+// parissiusti visus postus su fetch ar axios
+
+// atvaizduoti juos saraso pavidalu su nuorodom
+
+// kiekviena nuoroda veda i individualu posta
+
+// sukurti nauja lentele Authors
+
+// lenteleje turi buti stulpeliai
+// au_id INT Primary key, name VARCHAR, sex VARCHAR, age INT, post_id INT
+
+// Sukurti app.js /addAuthor endpointa i kuri nuejus sukuriam nauja autoriu(pan kaip newPost buvo)
+
+// sukurti atskira foma skirta autoriui kur galima sukurti nauja autoriu
+
+// sukurti 5 autorius skirtingais duomenimis
+
+// is fronto su fetch parsisiusti autorius ir atvaizduoti saraso pavidalu
+
+// virs saraso yra mygtukai : all authors(rodo visus ), young authors (rodo autorius kuriu amzius < 30), female authors
+// paspaudus mes is db parsiunciam atitinkamus duomenis ir juos atvaizduojam
