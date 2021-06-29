a06b99da971e8bd978057171c2338dc8fb06348a
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-29 09:39:20 +0300

    basic server + morgan

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // this is it
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('Express veikia normaliai');
+});
+
+app.listen('3001', console.log('Server running, port 3001'));
