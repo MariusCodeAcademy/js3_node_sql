f3b20ed746b0ed538b7caf9323201b284ba10863
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-30 09:57:16 +0300

    joinUzd

---
+++ static/productcs.js
@@ -0,0 +1,23 @@
+// pasikurti products.html faila
+
+// padaryti kad backendas paduotu products.html faila
+
+// per php my admin arba app.js susikurti products lentele
+// id, product_name, price, quantity, category (int susiejimui su product_categories.id)
+
+// susikurti lentele product_categories
+// id, name, product_id
+
+// sukurti 3 kategorijas 'home', 'outdoors', 'garden'
+
+// sukurti forma produktu sukurimui
+// formoj supildom product_name, price, quantity, category(selectas is esamu cat)
+
+// sukurti 7 + produktus panaudojant visas kategorijas
+
+// atvaizduoti saraso pavidalu prekes informacija su kategorija
+
+// atvaizduoti lenteles pavidalu prekes informacija su kategorija
+
+// virs lenteles sukurti select lauka su kategoriju pasirinkimais
+// pasirinkus kategorija sarase ir lenteleje rodom tik tos kategorijos duomenis
